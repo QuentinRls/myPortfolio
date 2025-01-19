db90828e2fdd462d5d729dfbd363e6f8a75a407a
quentin.riols@epitech.eu <quentin.riols@epitech.eu>
2025-01-19 18:47:46 +0100

    Updates

--- static/js/354.7131f516.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmyPortfolio=self.webpackChunkmyPortfolio||[]).push([[354],{354:(s,e,t)=>{t.r(e),t.d(e,{default:()=>o});t(791);var c=t(184);const o=()=>(0,c.jsx)("section",{id:"contact",className:"content",children:(0,c.jsx)("div",{className:"presText"})})}}]);
-//# sourceMappingURL=354.7131f516.chunk.js.map
